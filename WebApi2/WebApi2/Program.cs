08a10462bebe1be64b574fcd865b0c98d337cdc8
Shevyakov Nikita <shevyakov.n@promail366.com>
2026-05-08 17:30:34 +0300

    #initial web-api projects

---
+++ WebApi2/WebApi2/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.OpenApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(); // По умолчанию доступно по адресу /swagger
+}
+
+app.MapControllers();
+app.Run();
